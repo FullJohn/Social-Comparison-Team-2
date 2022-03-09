b057d31d4f1f086ade23e5b3804a7858c8413d27
John Fuller <49049456+FullJohn@users.noreply.github.com>
2022-03-09 04:17:12 -0700

    Updated Post.js
    Updated Post.js

--- SCReact/sc-react/src/Post.js
+++ SCReact/sc-react/src/Post.js
@@ -12,7 +12,7 @@ export class Post extends Component{
     refreshList(){
         const { queryId } = this.state
         fetch('http://54.144.107.206:8000/post/', {
-            method:'GET',
+            method:'POST',
             headers:{
                 'Accept':'application/json',
                 'Content-Type':'application/json'
