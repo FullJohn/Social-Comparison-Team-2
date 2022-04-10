a0708e4323fe4eeda0e09edb1631557529fb4ecd
John Fuller <49049456+FullJohn@users.noreply.github.com>
2022-04-10 11:31:50 -0600

    Update RenderPosts.js

--- SCReact/sc-react/src/RenderPosts.js
+++ SCReact/sc-react/src/RenderPosts.js
@@ -11,41 +11,41 @@ export function renderposts(props){
     const platform = props[0]
 
     const posts = []
-    switch(props.platform){
-        case 'Facebook':{
-            for(var i = 0; i < props[1].length; i++){
-                posts.push(FacebookFormat(props[1][i]))
-            }
+
+    if(platform == 'Facebook'){
+        for(var i = 0; i < props[1].length; i++){
+            posts.push(FacebookFormat(props[1][i]))
         }
-        case 'Instagram':{
-            for(var i = 0; i < props[1].length; i++){
-                posts.push(InstagramFormat(props[1][i]))
-            }
+    }
+
+    else if(platform == 'Instagram'){
+        for(var i = 0; i < props[1].length; i++){
+            posts.push(InstagramFormat(props[1][i]))
         }
-        case 'Pinterest':{
-            for(var i = 0; i < props[1].length; i++){
-                posts.push(PinterestFormat(props[1][i]))
-            }
+    }
+
+    else if(platform == 'Pinterest'){
+        for(var i = 0; i < props[1].length; i++){
+            posts.push(PinterestFormat(props[1][i]))
         }
-        case 'TikTok':{
-            for(var i = 0; i < props[1].length; i++){
-                posts.push(TiktokFormat(props[1][i]))
-            }
+    }
+
+    else if(platform == 'TikTok'){
+        for(var i = 0; i < props[1].length; i++){
+            posts.push(TiktokFormat(props[1][i]))
         }
-        case 'Twitter':{
-            for(var i = 0; i < props[1].length; i++){
-                posts.push(TwitterFormat(props[1][i]))
-            }
+    }
+
+    else if(platform == 'Twitter'){
+        for(var i = 0; i < props[1].length; i++){
+            posts.push(TwitterFormat(props[1][i]))
         }
-        case 'YouTube':{
-            for(var i = 0; i < props[1].length; i++){
-                posts.push(YouTubeFormat(props[1][i]))
-            }
+    }
+
+    else if(platform == 'YouTube'){
+        for(var i = 0; i < props[1].length; i++){
+            posts.push(YouTubeFormat(props[1][i]))
         }
-        default:
-            for(var i = 0; i < props[1].length; i++){
-                posts.push(YouTubeFormat(props[1][i]))
-            }
     }
     
     return(posts)
