52a4d010d06722beac82f232f5cf6932a0d3c2ea
John Fuller <49049456+FullJohn@users.noreply.github.com>
2022-04-10 14:27:19 -0600

    Update PostMetrics.js

--- SCReact/sc-react/src/PostMetrics.js
+++ SCReact/sc-react/src/PostMetrics.js
@@ -14,7 +14,7 @@ export function YouTubeMetrics(brandMetrics, post){
 export function RenderMetrics(metrics){
 
     return(
-        <Table className="Table-Display" variant="dark">
+        <Table className="Table-Display" bordered="true">
             
             <thead>
                 <tr>
