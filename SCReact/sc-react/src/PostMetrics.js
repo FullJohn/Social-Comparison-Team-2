c46b65b34a5b5a89cb7d5303e78765341f9064f5
John Fuller <49049456+FullJohn@users.noreply.github.com>
2022-04-10 14:16:06 -0600

    Formatting

--- SCReact/sc-react/src/PostMetrics.js
+++ SCReact/sc-react/src/PostMetrics.js
@@ -14,7 +14,7 @@ export function YouTubeMetrics(brandMetrics, post){
 export function RenderMetrics(metrics){
 
     return(
-        <Table>
+        <Table className="Table-Display">
             <div className='Total-Metrics'>
             <thead>
                 <tr>
