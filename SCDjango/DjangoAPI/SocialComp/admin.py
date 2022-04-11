734c918974d26133eb615df1bab1686aaab7b347
John Fuller <49049456+FullJohn@users.noreply.github.com>
2022-04-11 15:50:05 -0600

    Update admin.py

--- SCDjango/DjangoAPI/SocialComp/admin.py
+++ SCDjango/DjangoAPI/SocialComp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from SocialComp.models import QueryModel, QueryExecutedModel, PostModel, PostModel_Pinterest, PostModel_TikTok, PostModel_Twitter
 
+admin.site.register(QueryModel)
+admin.site.register(QueryExecutedModel)
+admin.site.register(PostModel)
+admin.site.register(PostModel_Twitter)
+admin.site.register(PostModel_Pinterest)
+admin.site.register(PostModel_TikTok)
 # Register your models here.
+
+
